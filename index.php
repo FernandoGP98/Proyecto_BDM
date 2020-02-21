13ecd45b79c2fefbcd131208e4c5c157069c791f
Fernando <fer_char98@hotmail.com>
2020-02-21 16:21:27 -0600

    Se agrego Slick carousel y diseño en el index

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -10,12 +11,113 @@
     <script src="bootstrap-4.4.1-dist/js/bootstrap.min.js"></script>
 
     <!--Fonst Awesome (Iconos, ejemplo F de facebook)-->
-    <link href="fontawesome-free-5.12.1-web/css/all.css" rel="stylesheet"> <!--load all styles -->
+    <link href="fontawesome-free-5.12.1-web/css/all.css" rel="stylesheet">
 
+    <!--Slick Carousel-->
+    <link rel="stylesheet" type="text/css" href="slick/slick.css" />
+    <!--Estilo default-->
+    <link rel="stylesheet" type="text/css" href="slick/slick-theme.css" />
+
+    <!--load styles -->
+    <link rel="stylesheet" href="css/index.css">
     <title>Document</title>
 </head>
+
 <body>
-    <?php include('templates/navbar.php');?>
-    <?php include('templates/footer.php');?>
+    <?php include 'templates/navbar.php';?>
+    <div class="container">
+        <div class="row d-flex justify-content-center">
+            <div class="col-12">
+                <hr class="nextSection">
+                <div class="row">
+                    <div class="col-12 text-center">
+                        <h2>Portada</h2>
+                    </div>
+                </div>
+                <hr class="inSection">
+                <div class="row d-flex justify-content-center">
+                    <div class="col-8">
+                        <div class="carousel">
+                            <div>
+                                <h1>1</h1>
+                            </div>
+                            <div>
+                                <h1>2</h1>
+                            </div>
+                            <div>
+                                <h1>3</h1>
+                            </div>
+                            <div>
+                                <h1>4</h1>
+                            </div>
+                            <div>
+                                <h1>5</h1>
+                            </div>
+                            <div>
+                                <h1>6</h1>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <hr class="inSection">
+            </div>
+        </div>
+        <?php
+        for ($i=0; $i < 5; $i++) { 
+        ?>
+        <div class="row d-flex justify-content-center">
+            <div class="col-12">
+                <hr class="nextSection">
+                <div class="row">
+                    <div class="col-12 text-center">
+                        <h2>Seccion<?=$i?></h2>
+                    </div>
+                </div>
+                <hr class="inSection">
+                <div class="row d-flex justify-content-center">
+                    <div class="col-8">
+                        <div class="carousel">
+                            <div>
+                                <h1>1</h1>
+                            </div>
+                            <div>
+                                <h1>2</h1>
+                            </div>
+                            <div>
+                                <h1>3</h1>
+                            </div>
+                            <div>
+                                <h1>4</h1>
+                            </div>
+                            <div>
+                                <h1>5</h1>
+                            </div>
+                            <div>
+                                <h1>6</h1>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <hr class="inSection">
+            </div>
+        </div>
+        <?php
+        }
+        ?>
+    </div>
+    <?php include 'templates/footer.php';?>
+
+    <!--Slick JS-->
+    <script type="text/javascript" src="slick/slick.min.js"></script>
+    <script>
+    $(document).ready(function() {
+        $('.carousel').slick({
+            slidesToShow: 3,
+            dots: true,
+            centerMode: true,
+        });
+    });
+    </script>
 </body>
+
 </html>
--- templates/navbar.php
+++ templates/navbar.php
@@ -1,35 +1,44 @@
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <a class="navbar-brand" href="#">Navbar</a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
+<link rel="stylesheet" href="css/navbar.css">
+<nav class="navbar navbar-expand-lg navbar-light bg-light navbar-center">
+    <a class="navbar-brand" href="#">Noticias</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+        aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
 
-  <div class="collapse navbar-collapse" id="navbarSupportedContent">
-    <ul class="navbar-nav mr-auto">
-      <li class="nav-item active">
-        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">Link</a>
-      </li>
-      <li class="nav-item dropdown">
-        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-          Dropdown
-        </a>
-        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-          <a class="dropdown-item" href="#">Action</a>
-          <a class="dropdown-item" href="#">Another action</a>
-          <div class="dropdown-divider"></div>
-          <a class="dropdown-item" href="#">Something else here</a>
-        </div>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link disabled" href="#">Disabled</a>
-      </li>
-    </ul>
-    <form class="form-inline my-2 my-lg-0">
-      <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-    </form>
-  </div>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <button class="btn btn-outline-succes my-2 my-sm-0">Ingresar</button>
+      <button class="btn btn-outline-succes my-2 my-sm-0">Registrarse</button>
+        <form class="form-inline my-2 my-lg-0">
+            <input style="display: none" class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+            <button class="btn btn-outline-success my-2 my-sm-0" type="submit"><i class="fas fa-search"> </i></button>
+        </form>
+    </div>
+</nav>
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav mr-auto">
+            <li class="nav-item active">
+                <a class="nav-link" href="#">Seccion1 <span class="sr-only"></span></a>
+            </li>
+            <li class="nav-item active">
+                <a class="nav-link" href="#">Seccion2 <span class="sr-only"></span></a>
+            </li>
+            <li class="nav-item active">
+                <a class="nav-link" href="#">Seccion3 <span class="sr-only"></span></a>
+            </li>
+            <li class="nav-item active">
+                <a class="nav-link" href="#">Seccion4 <span class="sr-only"></span></a>
+            </li>
+            <li class="nav-item active">
+                <a class="nav-link" href="#">Seccion5 <span class="sr-only"></span></a>
+            </li>
+            <li class="nav-item active">
+                <a class="nav-link" href="#">Seccion6 <span class="sr-only"></span></a>
+            </li>
+            <li class="nav-item active">
+                <a class="nav-link" href="#">Seccion7 <span class="sr-only"></span></a>
+            </li>
+        </ul>
+    </div>
 </nav>
