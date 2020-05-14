e6348b959cd7d15f9eb22ca1ae45152e813c6462
Jesus <whitesoul_15@hotmail.com>
2020-05-14 12:06:05 -0500

    Error en la pagina de Redactar no tomaba el JS

--- public/redactar.php
+++ public/redactar.php
@@ -6,6 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <?php include 'links.php';?>
+    <script src="resources/js/redactar.js"></script>
 </head>
 
 <body>
