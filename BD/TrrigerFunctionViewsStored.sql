546b78ca9430e827d859261c455e8b9a2535be26
Jesus <whitesoul_15@hotmail.com>
2020-05-22 20:58:04 -0500

    Trigger Funciones Views

--- BD/TrrigerFunctionViewsStored.sql
+++ BD/TrrigerFunctionViewsStored.sql
@@ -17,6 +17,41 @@ inner join noticiaimagen as NI
 on I.id_Imagen = NI.imagen;
 /*===================================================================================================*/
 /*===================================================================================================*/
+/* View - Noticia
+	- Trae toda la info de la noticia, firma del autor, nombre de la seccion, nombre de la palabra clave, direccion del video
+*/
+create view vVerNoticia as
+select N.id_Noticia, N.Titulo, N.FechaPublicacion, N.FechaAcontesimiento, N.Lugar, N.Descripcion, N.Texto, N.destacada, N.activa, N.seccion, S.seccion_nombre, 
+N.estatus, N.Autor, U.firma, n.palabra, p.PalabraClave,v.direccion_video 
+from noticia as N
+inner join noticiavideo as NV
+on n.id_Noticia = NV.noticia
+inner join video as V
+on V.id_Video = NV.video
+inner join seccion as S
+on S.id_Seccion = N.seccion
+inner join usuario as U
+on U.id_Usuario = n.Autor
+inner join palabraclave as P
+on P.id_PalabraClave = N.palabra;
+/*===================================================================================================*/
+/*===================================================================================================*/
+/*
+	Stored 
+	 - usa el View de arriba para obtener las imagenes de una noticia en especifico
+*/
+DELIMITER //
+CREATE PROCEDURE obtenerImagenesNoticia(
+	in idNoticia int
+)
+BEGIN
+	select id_Imagen, imagen, noticia
+    from vNoticiaImagenes
+    where noticia = idNoticia;
+END //
+DELIMITER ;
+/*===================================================================================================*/
+/*===================================================================================================*/
 /*Stored - Guardar Imagenes de noticia
 	- Hace un Registro en Imagen para despues hacer un Registro en NoticiaImagen
 */
@@ -66,17 +101,40 @@ END //
 DELIMITER ;
 /*===================================================================================================*/
 /*===================================================================================================*/
-/* Trigger - Borrar Comentarios de una Noticia(cambiar nombre)
+/* Trigger - Borrar Noticia(cambiar nombre)
 	- Antes de borrar una noticia Elimina todos los comntarios que pertenescan a ella
+    - Imagenes
+    - Video
 */
 delimiter =)
-create trigger triggerDeleteComentarios3
+create trigger tgBorrarNoticia
 before delete on noticia
 for each row
 begin
 	delete from comentarios where noticia = old.id_Noticia;
+    delete from noticiaimagen where noticia = old.id_Noticia;
+    delete from noticiavideo where noticia = old.id_Noticia;
 end =)
 delimiter ;
+/*===================================================================================================*/
+/*===================================================================================================*/
+/* view - Noticia cards
+	- version resumida de la noticia para todas las cards (incluye 1 imagen)
+*/
+create view vNoticiaCard as
+select n.id_Noticia, n.Titulo, n.FechaPublicacion, n.FechaAcontesimiento, n.Descripcion, n.destacada, n.activa, n.estatus, en.estatus as estatusNombre, n.seccion,p.PalabraClave, i.imagen, u.firma, n.autor
+from noticia as n
+inner join palabraclave as p
+on n.palabra = p.id_PalabraClave
+inner join noticiaimagen as ni
+on ni.noticia = n.id_noticia
+inner join imagen as i
+on i.id_imagen = ni.imagen
+inner join estatus_noticia as en
+on en.id_estatus_noticia = n.estatus
+inner join usuario as u
+on u.id_usuario = n.autor
+group by id_noticia;
 /*===================================================================================================*/
 /*===================================================================================================*/
 /*Trigger - Borrar Seccion (quita la llave foranea de la seccion en Noticia)
@@ -123,5 +181,25 @@ begin
     end if;
 end =)
 delimiter ;
+/*===================================================================================================*/
+/*===================================================================================================*/
+/*
+	Funsion - Ultima Palabra
+*/
+DELIMITER //
+CREATE FUNCTION ultimaPalabra() 
+RETURNS int 
+DETERMINISTIC
+BEGIN
+	declare palabra int;
+	
+    set palabra = (select id_PalabraClave 
+    from palabraclave 
+    order by id_PalabraClave desc limit 1);
+    
+    return (palabra);
+END 
+//
+DELIMITER ;
 /*===================================================================================================*/
 /*===================================================================================================*/
